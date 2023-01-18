5f52d7fd8f5daacffd396f205b152ee5e9a5cfdc
gabrielbazante <gabrielbazante7@gmail.com>
2023-01-18 13:30:01 -0300

    Hooks useContext 2.0

--- hooks/src/views/App.jsx
+++ hooks/src/views/App.jsx
@@ -1,25 +1,28 @@
 import './App.css'
 import React from 'react'
-import { BrowserRouter as Router } from  'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
 
-import  DataContext, { data } from '../data/DataContext'
+import DataContext, { data } from '../data/DataContext'
 import { useState } from 'react'
+import Store from '../data/Store'
 
 const App = props => {
     const [state, setState] = useState(data)
 
     return (
-        <DataContext.Provider value={{state, setState}}>
+        <Store>
+            <DataContext.Provider value={{ state, setState }}>
                 <div className="App">
                     <Router>
                         <Menu />
                         <Content />
                     </Router>
                 </div>
-        </DataContext.Provider> 
+            </DataContext.Provider>
+        </Store>
     )
 }
 
